9013dc9fd7f7c2aeedfcfd6eb63e85059539a95e
essbedev <bernardi.sylvain@gmail.com>
2020-10-28 11:05:57 +0100

    main controller

---
+++ Assets/Scripts/App/MainController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneController.Instance.LoadScene("Environment");
+        SceneController.Instance.LoadScene("Props");
+    }
+
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(10,10,100,40), "Unload Props")){
+            SceneController.Instance.UnloadScene("Props");
+        }
+    }
+}
---
+++ Assets/Scripts/Core/SceneController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : Singleton<SceneController>
+{
+    public void LoadScene(string scene, bool add = true){
+        SceneManager.LoadSceneAsync(scene, add ? LoadSceneMode.Additive : LoadSceneMode.Single);
+    }
+
+    public void UnloadScene(string scene){
+        SceneManager.UnloadSceneAsync(scene);
+    }
+}
