abbeee4a39b700eb549111c06a3a4301e45aeb43
essbedev <bernardi.sylvain@gmail.com>
2020-10-28 14:09:34 +0100

    fps player prefab

---
+++ Assets/FPSPlayer/MouseLook.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform player;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private float xRot = 0f;
+    void Update()
+    {
+        float xRotation = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float yRotation = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRot -= yRotation;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+        player.Rotate(Vector3.up * xRotation);
+    }
+}
---
+++ Assets/FPSPlayer/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public float speed = 10f;
+
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, .4f, groundLayer);
+
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move*speed*Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            velocity.y = Mathf.Sqrt(jumpHeight*-2f*gravity);
+        }
+
+        velocity.y += gravity*Time.deltaTime;
+        controller.Move(velocity*Time.deltaTime);
+    }
+}
