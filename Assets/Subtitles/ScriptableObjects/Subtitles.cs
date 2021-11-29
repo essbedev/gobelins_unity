72196d2c7036e1cd20d74f96271be0e9bf2806df
essbedev <bernardi.sylvain@gmail.com>
2021-11-29 14:25:47 +0100

    subtitles

---
+++ Assets/Subtitles/ScriptableObjects/Subtitles.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Subtitle/Subtitles")]
+public class Subtitles : ScriptableObject
+{
+
+    public delegate void SubtitleEvent(int currentLine);
+    public event SubtitleEvent OnSubtitleChange;
+    public event SubtitleEvent OnSubtitleDisplayed;
+
+    public List<string> Lines;
+
+    public bool IsDisplayed = false;
+
+    private int _currentLine;
+
+    public int GetCurrentLine(){
+        return _currentLine;
+    }
+
+    public void StartDisplay(){
+        _currentLine = 0;
+    }
+
+    public bool HasNext(){
+        return (_currentLine < Lines.Count-1);
+    }
+
+    public void Next(){
+        if(HasNext()){
+            _currentLine++;
+            if(OnSubtitleChange != null) OnSubtitleChange(_currentLine);
+        }else{
+            IsDisplayed = true;
+            if(OnSubtitleDisplayed != null) OnSubtitleDisplayed(_currentLine);
+        }
+    }
+
+}
---
+++ Assets/Subtitles/Scripts/SubtitleDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubtitleDisplay : MonoBehaviour
+{
+    public Subtitles subtitles;
+    private Text _text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text = GetComponent<Text>();
+        subtitles.StartDisplay();
+        subtitles.OnSubtitleDisplayed += OnDisplayed;
+        _text.text = subtitles.Lines[subtitles.GetCurrentLine()];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            subtitles.Next();
+            if(!subtitles.IsDisplayed){
+                _text.text = subtitles.Lines[subtitles.GetCurrentLine()];
+            }
+        }
+    }
+
+    private void OnDisplayed(int line){
+        _text.enabled=false;
+    }
+}
